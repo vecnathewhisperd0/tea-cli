10a9b72cdbe1eea699db2b5454ff618aa16725af
Andreas Ulm <andreas.ulm@root360.de>
2019-04-28 22:36:59 +0200

    initial cobra implementation
    Signed-off-by: Andreas Ulm <andreas.ulm@root360.de>

---
+++ cmd/root.go
@@ -0,0 +1,69 @@
+// Copyright © 2019 The Gitea Authors. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	homedir "github.com/mitchellh/go-homedir"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var cfgFile string
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "tea",
+	Short: "Command line tool to interact with Gitea",
+	Long: `Command line tool to interact with Gitea
+
+Tea is an application to interact with the Gitea API.`,
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.tea/tea.yaml)")
+}
+
+// initConfig reads in config file and ENV variables if set.
+func initConfig() {
+	if cfgFile != "" {
+		// Use config file from the flag.
+		viper.SetConfigFile(cfgFile)
+	} else {
+		// Find home directory.
+		home, err := homedir.Dir()
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		// Search config in home directory with name ".tea" (without extension).
+		viper.AddConfigPath(home + "/.tea")
+		viper.SetConfigName("tea")
+	}
+
+	viper.AutomaticEnv() // read in environment variables that match
+
+	// If a config file is found, read it in.
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Println("Using config file:", viper.ConfigFileUsed())
+	}
+}
--- go.mod
+++ go.mod
@@ -4,8 +4,12 @@ go 1.12
 
 require (
 	code.gitea.io/sdk v0.0.0-20190424055801-13a7bf625b83
+	github.com/BurntSushi/toml v0.3.1 // indirect
 	github.com/go-gitea/yaml v0.0.0-20170812160011-eb3733d160e7
+	github.com/inconshreveable/mousetrap v1.0.0 // indirect
+	github.com/mitchellh/go-homedir v1.0.0
+	github.com/spf13/cobra v0.0.3
+	github.com/spf13/viper v1.3.2
 	github.com/urfave/cli v1.20.0
 	gopkg.in/src-d/go-git.v4 v4.11.0
-	gopkg.in/yaml.v2 v2.2.2 // indirect
 )
