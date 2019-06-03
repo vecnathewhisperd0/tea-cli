2f7738121c6cb74e9deeee7652d323ee84d2b9ae
Sandro Santilli <strk@kbt.io>
2019-06-03 08:28:03 +0200

    First draft of a 'tea pulls create' command

--- cmd/pulls.go
+++ cmd/pulls.go
@@ -19,6 +19,9 @@ var CmdPulls = cli.Command{
 	Usage:       "Operate with pulls of the repository",
 	Description: `Operate with pulls of the repository`,
 	Action:      runPulls,
+	Subcommands: []cli.Command{
+		CmdPullsCreate,
+	},
 	Flags: []cli.Flag{
 		cli.StringFlag{
 			Name:  "login, l",
@@ -61,3 +64,66 @@ func runPulls(ctx *cli.Context) error {
 
 	return nil
 }
+
+// CmdPullsCreate represents a sub command of pulls to create pr
+var CmdPullsCreate = cli.Command{
+	Name:        "create",
+	Usage:       "Create a pull-request on repository",
+	Description: `Create a pull-request on repository`,
+	Action:      runCreatePullRequest,
+	Flags: []cli.Flag{
+		cli.StringFlag{
+			Name:  "head, h",
+			Usage: "pull-request head",
+		},
+		cli.StringFlag{
+			Name:  "base, b",
+			Usage: "pull-request base",
+		},
+		cli.StringFlag{
+			Name:  "title, t",
+			Usage: "pull-request title",
+		},
+		cli.StringFlag{
+			Name:  "description, d",
+			Usage: "pull-request description",
+		},
+	},
+}
+
+func runCreatePullRequest(ctx *cli.Context) error {
+	login, owner, repo := initCommand(ctx)
+
+/*
+  Head      string   `json:"head" binding:"Required"`
+  Base      string   `json:"base" binding:"Required"`
+  Title     string   `json:"title" binding:"Required"`
+  Body      string   `json:"body"`
+  Assignee  string   `json:"assignee"`
+  Assignees []string `json:"assignees"`
+  Milestone int64    `json:"milestone"`
+  Labels    []int64  `json:"labels"`
+  // swagger:strfmt date-time
+  Deadline *time.Time `json:"due_date"`
+*/
+
+	pr, err := login.Client().CreatePullRequest(owner, repo, gitea.CreatePullRequestOption{
+		Head: ctx.String("head"),
+    Base: ctx.String("base"),
+    Title: ctx.String("title"),
+    Body: ctx.String("body"),
+	})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("#%d %s\n%s created %s\n\n%s", pr.Index,
+		pr.Title,
+		pr.Poster.UserName,
+		pr.Created.Format("2006-01-02 15:04:05"),
+		pr.Body,
+	)
+	return nil
+}
+
