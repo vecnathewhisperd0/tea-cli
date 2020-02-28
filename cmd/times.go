8f15abba154abb738853390b4c48f555684ea60d
Norwin Roosen <git@nroo.de>
2020-02-28 16:19:10 +0100

    times: reword help

--- cmd/times.go
+++ cmd/times.go
@@ -20,8 +20,11 @@ import (
 // CmdTrackedTimes represents the command to operate repositories' times.
 var CmdTrackedTimes = cli.Command{
 	Name:        "times",
-	Usage:       "Operate on tracked times of the repository's issues",
-	Description: `Operate on tracked times of the repository's issues`,
+	Aliases: []string{"time"},
+	Usage:       "Operate on tracked times of a repository's issues & pulls",
+	Description: `Operate on tracked times of a repository's issues & pulls.
+		 Depending on your permissions on the repository, only your own tracked
+		 times might be listed.`,
 	ArgsUsage:   "[username | #issue]",
 	Action:      runTrackedTimes,
 	Subcommands: []*cli.Command{
