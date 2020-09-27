dd7727a55f74fc6d4a75cf5b6f0b3f4804881d47
6543 <6543@obermui.de>
2020-09-28 01:43:18 +0200

    Move Interactive Login Creation to interact package

--- cmd/login.go
+++ cmd/login.go
@@ -7,9 +7,8 @@ package cmd
 import (
 	"fmt"
 	"log"
-	"net/url"
-	"strings"
 
+	"code.gitea.io/tea/modules/interact"
 	"code.gitea.io/tea/modules/intern"
 
 	"github.com/skratchdot/open-golang/open"
@@ -145,77 +144,8 @@ func runLoginAdd(ctx *cli.Context) error {
 		ctx.Bool("insecure"))
 }
 
-func runLoginAddInteractive(ctx *cli.Context) error {
-	var stdin, name, token, user, passwd, sshKey, giteaURL string
-	var insecure = false
-
-	fmt.Print("URL of Gitea instance: ")
-	if _, err := fmt.Scanln(&stdin); err != nil {
-		stdin = ""
-	}
-	giteaURL = strings.TrimSpace(stdin)
-	if len(giteaURL) == 0 {
-		fmt.Println("URL is required!")
-		return nil
-	}
-
-	parsedURL, err := url.Parse(giteaURL)
-	if err != nil {
-		return err
-	}
-	name = strings.ReplaceAll(strings.Title(parsedURL.Host), ".", "")
-
-	fmt.Print("Name of new Login [" + name + "]: ")
-	if _, err := fmt.Scanln(&stdin); err != nil {
-		stdin = ""
-	}
-	if len(strings.TrimSpace(stdin)) != 0 {
-		name = strings.TrimSpace(stdin)
-	}
-
-	fmt.Print("Do you have a token [Yes/no]: ")
-	if _, err := fmt.Scanln(&stdin); err != nil {
-		stdin = ""
-	}
-	if len(stdin) != 0 && strings.ToLower(stdin[:1]) == "n" {
-		fmt.Print("Username: ")
-		if _, err := fmt.Scanln(&stdin); err != nil {
-			stdin = ""
-		}
-		user = strings.TrimSpace(stdin)
-
-		fmt.Print("Password: ")
-		if _, err := fmt.Scanln(&stdin); err != nil {
-			stdin = ""
-		}
-		passwd = strings.TrimSpace(stdin)
-	} else {
-		fmt.Print("Token: ")
-		if _, err := fmt.Scanln(&stdin); err != nil {
-			stdin = ""
-		}
-		token = strings.TrimSpace(stdin)
-	}
-
-	fmt.Print("Set Optional settings [yes/No]: ")
-	if _, err := fmt.Scanln(&stdin); err != nil {
-		stdin = ""
-	}
-	if len(stdin) != 0 && strings.ToLower(stdin[:1]) == "y" {
-		fmt.Print("SSH Key Path: ")
-		if _, err := fmt.Scanln(&stdin); err != nil {
-			stdin = ""
-		}
-		sshKey = strings.TrimSpace(stdin)
-
-		fmt.Print("Allow Insecure connections  [yes/No]: ")
-		if _, err := fmt.Scanln(&stdin); err != nil {
-			stdin = ""
-		}
-		insecure = len(stdin) != 0 && strings.ToLower(stdin[:1]) == "y"
-	}
-
-	return intern.AddLogin(name, token, user, passwd, sshKey, giteaURL, insecure)
+func runLoginAddInteractive(_ *cli.Context) error {
+	return interact.CreateLogin()
 }
 
 // CmdLogin represents to login a gitea server.
