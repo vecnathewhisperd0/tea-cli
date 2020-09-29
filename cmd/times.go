efc75ca394d0bda7ea551e63744e4aa71d7e9f48
6543 <6543@obermui.de>
2020-09-29 13:26:52 +0200

    Move print TrackedTimesList to print package

--- cmd/times.go
+++ cmd/times.go
@@ -6,8 +6,6 @@ package cmd
 
 import (
 	"fmt"
-	"log"
-	"strconv"
 	"strings"
 	"time"
 
@@ -102,60 +100,6 @@ func runTrackedTimes(ctx *cli.Context) error {
 		}
 	}
 
-	printTrackedTimes(times, flags.GlobalOutputValue, from, until, ctx.Bool("total"))
+	print.TrackedTimesList(times, flags.GlobalOutputValue, from, until, ctx.Bool("total"))
 	return nil
-}
-
-func formatDuration(seconds int64, outputType string) string {
-	switch outputType {
-	case "yaml":
-	case "csv":
-		return fmt.Sprint(seconds)
-	}
-	return time.Duration(1e9 * seconds).String()
-}
-
-func printTrackedTimes(times []*gitea.TrackedTime, outputType string, from, until time.Time, printTotal bool) {
-	var outputValues [][]string
-	var totalDuration int64
-
-	localLoc, err := time.LoadLocation("Local") // local timezone for time formatting
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	for _, t := range times {
-		if !from.IsZero() && from.After(t.Created) {
-			continue
-		}
-		if !until.IsZero() && until.Before(t.Created) {
-			continue
-		}
-
-		totalDuration += t.Time
-
-		outputValues = append(
-			outputValues,
-			[]string{
-				t.Created.In(localLoc).Format("2006-01-02 15:04:05"),
-				"#" + strconv.FormatInt(t.Issue.Index, 10),
-				t.UserName,
-				formatDuration(t.Time, outputType),
-			},
-		)
-	}
-
-	if printTotal {
-		outputValues = append(outputValues, []string{
-			"TOTAL", "", "", formatDuration(totalDuration, outputType),
-		})
-	}
-
-	headers := []string{
-		"Created",
-		"Issue",
-		"User",
-		"Duration",
-	}
-	print.OutputList(outputType, headers, outputValues)
 }
