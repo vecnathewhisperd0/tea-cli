d6b49427c90a258b65f0d8905928509edde60c6a
6543 <6543@obermui.de>
2020-12-07 00:51:57 +0100

    MV list labels -> print.LabelsList & task.LabelsExport

---
+++ modules/print/label.go
@@ -0,0 +1,49 @@
+// Copyright 2020 The Gitea Authors. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
+package print
+
+import (
+	"fmt"
+	"strconv"
+
+	"code.gitea.io/tea/cmd/flags"
+
+	"code.gitea.io/sdk/gitea"
+	"github.com/muesli/termenv"
+)
+
+// LabelsList prints a listing of issues
+func LabelsList(labels []*gitea.Label) {
+	headers := []string{
+		"Index",
+		"Color",
+		"Name",
+		"Description",
+	}
+
+	var values [][]string
+
+	if len(labels) == 0 {
+		OutputList(flags.GlobalOutputValue, headers, values)
+		return
+	}
+
+	p := termenv.ColorProfile()
+
+	for _, label := range labels {
+		color := termenv.String(label.Color)
+
+		values = append(
+			values,
+			[]string{
+				strconv.FormatInt(label.ID, 10),
+				fmt.Sprint(color.Background(p.Color("#" + label.Color))),
+				label.Name,
+				label.Description,
+			},
+		)
+	}
+	OutputList(flags.GlobalOutputValue, headers, values)
+}
