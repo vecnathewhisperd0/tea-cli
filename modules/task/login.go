7267cd0afb97f936f5c203ff4e20c4777abc1fe6
6543 <6543@obermui.de>
2020-12-11 14:54:42 +0100

    move FindSSHKey to task module

--- modules/task/login.go
+++ modules/task/login.go
@@ -5,15 +5,20 @@
 package task
 
 import (
+	"encoding/base64"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
+	"path/filepath"
+	"strings"
 	"time"
 
 	"code.gitea.io/tea/modules/config"
 	"code.gitea.io/tea/modules/utils"
 
 	"code.gitea.io/sdk/gitea"
+	"golang.org/x/crypto/ssh"
 )
 
 // CreateLogin create a login to be stored in config
@@ -57,15 +62,17 @@ func CreateLogin(name, token, user, passwd, sshKey, giteaURL string, insecure bo
 		Created:  time.Now().Unix(),
 	}
 
+	client := login.Client()
+
 	if len(token) == 0 {
-		login.Token, err = generateToken(login.Client(), user, passwd)
+		login.Token, err = generateToken(client, user, passwd)
 		if err != nil {
 			log.Fatal(err)
 		}
 	}
 
 	// Verify if authentication works and get user info
-	u, _, err := login.Client().GetMyUserInfo()
+	u, _, err := client.GetMyUserInfo()
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -83,7 +90,7 @@ func CreateLogin(name, token, user, passwd, sshKey, giteaURL string, insecure bo
 	login.SSHHost = serverURL.Hostname()
 
 	if len(sshKey) == 0 {
-		login.SSHKey, err = login.FindSSHKey()
+		login.SSHKey, err = findSSHKey(client)
 		if err != nil {
 			fmt.Printf("Warning: problem while finding a SSH key: %s\n", err)
 		}
@@ -137,4 +144,66 @@ func GenerateLoginName(url, user string) (string, error) {
 	}
 
 	return name, nil
+}
+
+// findSSHKey retrieves the ssh keys registered in gitea, and tries to find
+// a matching private key in ~/.ssh/. If no match is found, path is empty.
+func findSSHKey(client *gitea.Client) (string, error) {
+	// get keys registered on gitea instance
+	keys, _, err := client.ListMyPublicKeys(gitea.ListPublicKeysOptions{})
+	if err != nil || len(keys) == 0 {
+		return "", err
+	}
+
+	// enumerate ~/.ssh/*.pub files
+	glob, err := utils.AbsPathWithExpansion("~/.ssh/*.pub")
+	if err != nil {
+		return "", err
+	}
+	localPubkeyPaths, err := filepath.Glob(glob)
+	if err != nil {
+		return "", err
+	}
+
+	// parse each local key with present privkey & compare fingerprints to online keys
+	for _, pubkeyPath := range localPubkeyPaths {
+		var pubkeyFile []byte
+		pubkeyFile, err = ioutil.ReadFile(pubkeyPath)
+		if err != nil {
+			continue
+		}
+		fields := strings.Split(string(pubkeyFile), " ")
+		if len(fields) < 2 { // first word is key type, second word is key material
+			continue
+		}
+
+		var keymaterial []byte
+		keymaterial, err = base64.StdEncoding.DecodeString(fields[1])
+		if err != nil {
+			continue
+		}
+
+		var pubkey ssh.PublicKey
+		pubkey, err = ssh.ParsePublicKey(keymaterial)
+		if err != nil {
+			continue
+		}
+
+		privkeyPath := strings.TrimSuffix(pubkeyPath, ".pub")
+		var exists bool
+		exists, err = utils.FileExist(privkeyPath)
+		if err != nil || !exists {
+			continue
+		}
+
+		// if pubkey fingerprints match, return path to corresponding privkey.
+		fingerprint := ssh.FingerprintSHA256(pubkey)
+		for _, key := range keys {
+			if fingerprint == key.Fingerprint {
+				return privkeyPath, nil
+			}
+		}
+	}
+
+	return "", err
 }
