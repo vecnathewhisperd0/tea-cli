b7ede03f527224b1ce6999dfc1c891518c9fbf54
6543 <6543@obermui.de>
2021-03-05 12:29:36 +0100

    Intorduce workaround for missing pull head sha

---
+++ modules/workaround/pull.go
@@ -0,0 +1,25 @@
+// Copyright 2020 The Gitea Authors. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
+package workaround
+
+import (
+	"net/url"
+
+	"code.gitea.io/sdk/gitea"
+)
+
+// FixPullHeadSha is a workaround for https://github.com/go-gitea/gitea/issues/12675
+func FixPullHeadSha(client *gitea.Client, pr *gitea.PullRequest, repoOwner, repoName string) error {
+	if pr.Head != nil && pr.Head.Sha == "" {
+		headCommit, resp, err := client.GetSingleCommit(repoOwner, repoName, url.PathEscape(pr.Head.Ref))
+		if err != nil && resp == nil || err != nil && resp.StatusCode != 404 {
+			return err
+		}
+		if headCommit != nil {
+			pr.Head.Sha = headCommit.SHA
+		}
+	}
+	return nil
+}
