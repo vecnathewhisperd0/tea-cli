7a7529f475cde2cc44004b5af3b5353a99e001e1
Norwin <git@nroo.de>
2021-09-28 12:50:11 +0200

    add `tea org create`

--- cmd/organizations.go
+++ cmd/organizations.go
@@ -23,6 +23,7 @@ var CmdOrgs = cli.Command{
 	Action:      runOrganizations,
 	Subcommands: []*cli.Command{
 		&organizations.CmdOrganizationList,
+		&organizations.CmdOrganizationCreate,
 		&organizations.CmdOrganizationDelete,
 	},
 	Flags: organizations.CmdOrganizationList.Flags,
