344bd6ce74c0e5de6a14948920f61dfc869e5555
Norwin <git@nroo.de>
2021-09-28 12:28:06 +0200

    move clone logic into task.RepoClone()

--- cmd/repos/clone.go
+++ cmd/repos/clone.go
@@ -5,18 +5,11 @@
 package repos
 
 import (
-	"fmt"
-	"net/url"
-
-	"code.gitea.io/sdk/gitea"
 	"code.gitea.io/tea/cmd/flags"
-	"code.gitea.io/tea/modules/config"
 	"code.gitea.io/tea/modules/context"
-	"code.gitea.io/tea/modules/git"
 	"code.gitea.io/tea/modules/interact"
+	"code.gitea.io/tea/modules/task"
 
-	git_config "github.com/go-git/go-git/v5/config"
-	"github.com/go-git/go-git/v5/plumbing"
 	"github.com/urfave/cli/v2"
 )
 
@@ -41,64 +34,14 @@ func runRepoClone(cmd *cli.Context) error {
 	ctx := context.InitCommand(cmd)
 	ctx.Ensure(context.CtxRequirement{RemoteRepo: true})
 
-	repoMeta, _, err := ctx.Login.Client().GetRepo(ctx.Owner, ctx.Repo)
-	if err != nil {
-		return err
-	}
-
-	originURL, err := cloneURL(repoMeta, ctx.Login)
-	if err != nil {
-		return err
-	}
-
-	auth, err := git.GetAuthForURL(originURL, ctx.Login.Token, ctx.Login.SSHKey, interact.PromptPassword)
-	if err != nil {
-		return err
-	}
-
-	// default path behaviour as native git
-	localPath := ctx.Args().First()
-	if localPath == "" {
-		localPath = ctx.Repo
-	}
-
-	repo, err := git.CloneIntoPath(
-		originURL.String(),
-		localPath,
-		auth,
+	_, err := task.RepoClone(
+		ctx.Args().First(),
+		ctx.Login,
+		ctx.Owner,
+		ctx.Repo,
+		interact.PromptPassword,
 		ctx.Int("depth"),
-		ctx.Login.Insecure,
 	)
 
-	// set up upstream remote for forks
-	if repoMeta.Fork && repoMeta.Parent != nil {
-		upstreamURL, err := cloneURL(repoMeta.Parent, ctx.Login)
-		if err != nil {
-			return err
-		}
-		upstreamBranch := repoMeta.Parent.DefaultBranch
-		repo.CreateRemote(&git_config.RemoteConfig{
-			Name: "upstream",
-			URLs: []string{upstreamURL.String()},
-		})
-		repoConf, err := repo.Config()
-		if err != nil {
-			return err
-		}
-		if b, ok := repoConf.Branches[upstreamBranch]; ok {
-			b.Remote = "upstream"
-			b.Merge = plumbing.ReferenceName(fmt.Sprintf("refs/heads/%s", upstreamBranch))
-		}
-		return repo.SetConfig(repoConf)
-	}
-
 	return err
-}
-
-func cloneURL(repo *gitea.Repository, login *config.Login) (*url.URL, error) {
-	urlStr := repo.CloneURL
-	if login.SSHKey != "" {
-		urlStr = repo.SSHURL
-	}
-	return git.ParseURL(urlStr)
 }
