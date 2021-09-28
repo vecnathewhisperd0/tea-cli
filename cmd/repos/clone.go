b1efcc7a09be8538063ad8a0447b8ff0e6da8395
Norwin <git@nroo.de>
2021-09-28 12:06:54 +0200

    set up upstream remote for forks

--- cmd/repos/clone.go
+++ cmd/repos/clone.go
@@ -5,13 +5,18 @@
 package repos
 
 import (
+	"fmt"
 	"net/url"
 
+	"code.gitea.io/sdk/gitea"
 	"code.gitea.io/tea/cmd/flags"
+	"code.gitea.io/tea/modules/config"
 	"code.gitea.io/tea/modules/context"
 	"code.gitea.io/tea/modules/git"
 	"code.gitea.io/tea/modules/interact"
 
+	git_config "github.com/go-git/go-git/v5/config"
+	"github.com/go-git/go-git/v5/plumbing"
 	"github.com/urfave/cli/v2"
 )
 
@@ -36,22 +41,17 @@ func runRepoClone(cmd *cli.Context) error {
 	ctx := context.InitCommand(cmd)
 	ctx.Ensure(context.CtxRequirement{RemoteRepo: true})
 
-	// get clone URLs robustly
-	repo, _, err := ctx.Login.Client().GetRepo(ctx.Owner, ctx.Repo)
-	if err != nil {
-		return err
-	}
-	var url *url.URL
-	urlStr := repo.CloneURL
-	if ctx.Login.SSHKey != "" {
-		urlStr = repo.SSHURL
-	}
-	url, err = git.ParseURL(urlStr)
+	repoMeta, _, err := ctx.Login.Client().GetRepo(ctx.Owner, ctx.Repo)
 	if err != nil {
 		return err
 	}
 
-	auth, err := git.GetAuthForURL(url, ctx.Login.Token, ctx.Login.SSHKey, interact.PromptPassword)
+	originURL, err := cloneURL(repoMeta, ctx.Login)
+	if err != nil {
+		return err
+	}
+
+	auth, err := git.GetAuthForURL(originURL, ctx.Login.Token, ctx.Login.SSHKey, interact.PromptPassword)
 	if err != nil {
 		return err
 	}
@@ -62,13 +62,43 @@ func runRepoClone(cmd *cli.Context) error {
 		localPath = ctx.Repo
 	}
 
-	_, err = git.CloneIntoPath(
-		url.String(),
+	repo, err := git.CloneIntoPath(
+		originURL.String(),
 		localPath,
 		auth,
 		ctx.Int("depth"),
 		ctx.Login.Insecure,
 	)
 
+	// set up upstream remote for forks
+	if repoMeta.Fork && repoMeta.Parent != nil {
+		upstreamURL, err := cloneURL(repoMeta.Parent, ctx.Login)
+		if err != nil {
+			return err
+		}
+		upstreamBranch := repoMeta.Parent.DefaultBranch
+		repo.CreateRemote(&git_config.RemoteConfig{
+			Name: "upstream",
+			URLs: []string{upstreamURL.String()},
+		})
+		repoConf, err := repo.Config()
+		if err != nil {
+			return err
+		}
+		if b, ok := repoConf.Branches[upstreamBranch]; ok {
+			b.Remote = "upstream"
+			b.Merge = plumbing.ReferenceName(fmt.Sprintf("refs/heads/%s", upstreamBranch))
+		}
+		return repo.SetConfig(repoConf)
+	}
+
 	return err
+}
+
+func cloneURL(repo *gitea.Repository, login *config.Login) (*url.URL, error) {
+	urlStr := repo.CloneURL
+	if login.SSHKey != "" {
+		urlStr = repo.SSHURL
+	}
+	return git.ParseURL(urlStr)
 }
