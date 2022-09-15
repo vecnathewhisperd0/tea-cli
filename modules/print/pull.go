d84d5fcbbc0a42bf932b41a78c4f649b6ccd9108
6543 <6543@obermui.de>
2022-09-15 04:06:10 +0200

    Add TODOs

--- modules/print/pull.go
+++ modules/print/pull.go
@@ -26,6 +26,7 @@ func PullDetails(pr *gitea.PullRequest, reviews []*gitea.PullReview, ciStatus *g
 	state := formatPRState(pr)
 
 	out := fmt.Sprintf(
+		// TODO: "# #%d %s (%s)\n@%s created %s\t**%s** <- **%s**\n\nAllow maintainer to edit: %v\n\n%s\n\n",
 		"# #%d %s (%s)\n@%s created %s\t**%s** <- **%s**\n\n%s\n\n",
 		pr.Index,
 		pr.Title,
@@ -34,6 +35,7 @@ func PullDetails(pr *gitea.PullRequest, reviews []*gitea.PullReview, ciStatus *g
 		FormatTime(*pr.Created, false),
 		base,
 		head,
+		// TODO: pr.AllowMaintainerEdit,
 		pr.Body,
 	)
 
