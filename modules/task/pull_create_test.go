6da66a03181d72e9c2ca396d8a41819767539efc
appleboy <appleboy.tw@gmail.com>
2023-04-29 08:10:32 +0800

    refactor: optimize PR title generation and expand test coverage
    - Import "regexp" package in pull_create.go
    - Replace existing code for `GetDefaultPRTitle` with a more efficient regular expression approach
    - Add a new test file pull_create_test.go with a test function for `GetDefaultPRTitle`
    
    Signed-off-by: appleboy <appleboy.tw@gmail.com>

---
+++ modules/task/pull_create_test.go
@@ -0,0 +1,24 @@
+package task
+
+import "testing"
+
+func TestGetDefaultPRTitle(t *testing.T) {
+	tests := []struct {
+		input string
+		want  string
+	}{
+		{input: "Add new feature", want: "Add New Feature"},
+		{input: "update-docs: Fix typo", want: "Fix Typo"},
+		{input: "remove_long-string", want: "Remove Long String"},
+		{input: "Replace_Underscores_With_Spaces", want: "Replace Underscores With Spaces"},
+		{input: "  leading-and-trailing-spaces ", want: "Leading And Trailing Spaces"},
+		{input: "-----No--Upper--Case-----", want: "No Upper Case"},
+		{input: "", want: ""},
+	}
+	for _, test := range tests {
+		got := GetDefaultPRTitle(test.input)
+		if got != test.want {
+			t.Errorf("GetDefaultPRTitle(%q) = %q, want %q", test.input, got, test.want)
+		}
+	}
+}
